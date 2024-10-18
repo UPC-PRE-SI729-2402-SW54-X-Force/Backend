5ee6625c1706d365ddf0a94fca6f441544e6a81b
Al3xiel <gabriel280104casas@gmail.com>
2024-10-17 23:20:16 -0500

    chore: initial commit.

---
+++ src/main/java/xforce/drivenowbackend/DrivenowBackendApplication.java
@@ -0,0 +1,13 @@
+package xforce.drivenowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivenowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivenowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xforce/drivenowbackend/DrivenowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xforce.drivenowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivenowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
